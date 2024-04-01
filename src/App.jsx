fe1c35693251f2d2a3d1e049d0f9c7172f387220
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-04-01 20:29:39 +0300

    01.04

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
@@ -9,7 +9,16 @@ import { nanoid } from "nanoid";
 const LS_KEY = "saved-contscts";
 
 function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  const [contacts, setContacts] = useState(() => {
+    const stringifiedContact = localStorage.getItem(LS_KEY);
+    return JSON.parse(stringifiedContact) || initialContacts;
+  });
+
+  const [search, setSearch] = useState(" ");
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   const onAddContact = (formData) => {
     const finalContacts = {
@@ -26,12 +35,16 @@ function App() {
     );
   };
 
+  const filteredContact = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm onAddContact={onAddContact} />
-      <SearchBox />
-      <ContactList contacts={contacts} onDelete={onDelete} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={filteredContact} onDelete={onDelete} />
     </div>
   );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,14 @@
-const SearchBox = () => {
-  return <div>SearchBox</div>;
+const SearchBox = ({ value, onSearch }) => {
+  return (
+    <div>
+      <p>Search by name</p>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => onSearch(event.target.value)}
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
