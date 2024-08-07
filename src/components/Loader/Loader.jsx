3624959a952cb35e1fc89e52a906b18a3945ef13
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-08-07 23:48:02 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,18 @@
-
 import { Bars } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div>
-      render(
+    <div className={css.loader}>
       <Bars
         height="80"
         width="80"
-        color="#4fa94d"
+        color="#008080"
         ariaLabel="bars-loading"
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
       />
-      )
     </div>
   );
 };
